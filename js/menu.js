17ebebb4915d5cef9257652dfa352f7199fcbf4b
AJTimePyro <ajtimepyro@gmail.com>
2021-09-01 23:38:22 +0530

    first commit

---
+++ js/menu.js
@@ -0,0 +1,21 @@
+
+// Home Button
+document.getElementById('box1').addEventListener('click',
+    function (evt) {
+        document.location.href = 'index.html';
+    }
+)
+
+// About Button
+document.getElementById('box2').addEventListener('click',
+    function (evt) {
+        document.location.href = 'about.html';
+    }
+)
+
+// Contact Us Button
+document.getElementById('contact').addEventListener('click',
+    function (evt) {
+        document.location.href = 'contact.html'
+    }
+)
