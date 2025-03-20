56e4465db30a35176b993260b13fed46bbabb634
AJTimePyro <ajtimepyro@gmail.com>
2025-03-20 17:38:20 +0530

    for deployment to github pages

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",  // <=== enables static exports
+  reactStrictMode: true,
 };
 
 export default nextConfig;
